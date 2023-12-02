2524c5978e45839472c72c40c5b68517bbc5c592
cvaisnor <chrisvaisnor@gmail.com>
2023-12-01 18:17:14 -0800

    complete restructuring

---
+++ ros2_pca9685/ros2_pca9685/pca9685_node.py
@@ -0,0 +1,43 @@
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+from adafruit_servokit import ServoKit
+
+default_steering_angle = 90
+steering_range = [50, 130]
+throttle_values = [0.00, 0.08]
+
+class PCA9685Node(Node):
+    def __init__(self):
+        super().__init__('pca9685_node')
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.subscription = self.create_subscription(Twist, 'cmd_vel', self.listener_callback, 10)
+
+        kit = ServoKit(channels=16, i2c=i2c_bus, address=0x40)
+        self.steering.Angle = kit.servo[0]
+        self.throttle = kit.continuous_servo[1]
+        self.steering_angle = default_steering_angle
+
+    def listener_callback(self, msg):
+        throttle_value = msg.linear.x
+        angle = msg.angular.z
+        if throttle_value > 0:
+            self.throttle.throttle = throttle_values[1]
+        else:
+            self.throttle.throttle = throttle_values[0]
+
+        if angle > 0:
+            self.steering_angle = max(min(steering_range[1], self.steering_angle + angle), steering_range[0])
+        elif angle < 0:
+            self.steering_angle = max(min(steering_range[1], self.steering_angle + angle), steering_range[0])
+
+        self.steering_angle.angle = self.steering_angle
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 0.0
+        msg.angular.z = 0.0
+        self.publisher_.publish(msg)
--- ros2_pca9685/setup.py
+++ ros2_pca9685/setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup
 
-package_name = 'ros2_pca9685'
+package_name = 'pca9685_ros2'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+                ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='main',
-    maintainer_email='stevej52@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='ROS2 interface for the PCA9685 servo controller',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-             'talker = ros2_pca9685.publisher_member_function:main',
-             'listener = ros2_pca9685.subscriber_member_function:main',
+            'pca9685 = pca9685_ros2.pca9685_node:main'
         ],
     },
 )
