be31eb7c2b918e0d5cffa36b4c470e8f2b2fb4a0
Chris Vaisnor <chrisvaisnor@gmail>
2023-12-01 22:47:41 -0800

    subscriber working

--- pca9685_ros2/pca9685_ros2/pca_node.py
+++ pca9685_ros2/pca9685_ros2/pca_node.py
@@ -6,23 +6,22 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from adafruit_servokit import ServoKit
 
-default_steering_angle = 90.0
+DEFAULT_STEERING_ANGLE = 90.0
 steering_range = [50.0, 130.0]
 throttle_values = [-0.08, 0.00, 0.08] # [reverse, neutral, forward]
 
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 
-# create a class for the PCA9685 node that subscribes to cmd_vel and sets the steering and throttle accordingly
 class PCA9685Node(Node):
     def __init__(self):
         super().__init__('pca9685_node')
-        self.get_logger().info('Initializing PCA9685 Node')
+        self.get_logger().info('Initializing PCA9685 Listener Node')
         self.subscription = self.create_subscription(Twist, 'cmd_vel', self.listener_callback, 10)
 
         kit = ServoKit(channels=16, i2c=i2c_bus, address=0x40)
         self.steering_servo = kit.servo[0]
         self.throttle_servo = kit.continuous_servo[1]
-        self.steering_servo.angle = default_steering_angle
+        self.steering_servo.angle = DEFAULT_STEERING_ANGLE
         self.throttle_servo.throttle = throttle_values[1]
 
     def listener_callback(self, msg):
@@ -34,13 +33,6 @@ class PCA9685Node(Node):
         self.steering_servo.angle = steering_angle
         self.throttle_servo.throttle = throttle_value
 
-    # def timer_callback(self):
-    #     msg = Twist()
-    #     msg.linear.x = throttle_values[1]
-    #     msg.angular.z = default_steering_angle
-        # self.publisher_.publish(msg)
-        # self.get_logger().info(f'Publishing (linear.x, angular.z): ({msg.linear.x}, {msg.angular.z})')
-
 
 def main(args=None):
 
@@ -49,7 +41,7 @@ def main(args=None):
     rclpy.spin(pca9685_node)
 
     # on shutdown, set steering and throttle to neutral position
-    pca9685_node.steering_servo.angle = default_steering_angle
+    pca9685_node.steering_servo.angle = DEFAULT_STEERING_ANGLE
     pca9685_node.throttle_servo.throttle = throttle_values[1]
 
     pca9685_node.destroy_node()
--- pca9685_ros2/setup.py
+++ pca9685_ros2/setup.py
@@ -4,23 +4,22 @@ package_name = 'pca9685_ros2'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-                ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Chris Vaisnor',
-    maintainer_email='chrisvaisnor@gmail.com',
-    description='ROS2 interface for the PCA9685 servo controller',
-    license='Apache License 2.0',
+    maintainer='chris',
+    maintainer_email='chrisvaisnor@gmail.',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-            'pca9685 = pca9685_ros2.pca9685_node:main',
+        'console_scripts': [ 'pca9685_subscriber = pca9685_ros2.pca_node:main'
         ],
     },
 )
